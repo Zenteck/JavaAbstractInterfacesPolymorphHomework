3c5ec45ac35230a765910c5ccbfe5b21a1fa956a
Alan <spambox+github@zenteck.co.uk>
2020-02-04 22:40:04 +0000

    It's after half 10...

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,25 @@
-public class ThemePark {
+import behaviours.IReviewed;
+import java.util.ArrayList;
+
+public class ThemePark{
+
+    private ArrayList<IReviewed> entertainments;
+
+    public ThemePark(){
+        this.entertainments = new ArrayList<IReviewed>();
+    }
+
+    public void addEntertainment(IReviewed entertainment){
+        this.entertainments.add(entertainment);
+    }
+
+    public int getEntertainmentCount(){
+        return this.entertainments.size();
+    }
+
+    public ArrayList<IReviewed> getAllReviewed() {
+        return this.entertainments;
+    }
+
+
 }
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,58 @@
+import attractions.Dodgems;
+import attractions.Park;
+import attractions.Playground;
+import attractions.RollerCoaster;
+import org.junit.Before;
+import org.junit.Test;
+import stalls.CandyflossStall;
+import stalls.IceCreamStall;
+import stalls.ParkingSpot;
+import stalls.TobaccoStall;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThemeParkTest {
+
+    private ThemePark themePark;
+    private Dodgems dodgems;
+    private Park park;
+    private Playground playground;
+    private RollerCoaster rollerCoaster;
+    private CandyflossStall candyflossStall;
+    private IceCreamStall iceCreamStall;
+    private TobaccoStall tobaccoStall;
+
+
+    @Before
+    public void setUp(){
+        themePark = new ThemePark();
+        dodgems = new Dodgems("Bumper Cars", 5);
+        park = new Park("Leafy Meadows", 9);
+        playground = new Playground("Fun Zone", 7);
+        rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        candyflossStall = new CandyflossStall("Candy Land", 2, "Harry Belafonte", ParkingSpot.A1);
+        iceCreamStall = new IceCreamStall("Dream Cones",3, "Vanilla Ice", ParkingSpot.A4);
+        tobaccoStall = new TobaccoStall("Jacks Drum", 5, "Jack Jarvis", ParkingSpot.B1);
+    }
+
+    @Test
+    public void canAddEntertainment(){
+        themePark.addEntertainment(dodgems);
+        assertEquals(1, themePark.getEntertainmentCount());
+    }
+
+    @Test
+    public void canGetAllReviewed(){
+        themePark.addEntertainment(dodgems);
+        themePark.addEntertainment(park);
+        themePark.addEntertainment(playground);
+        themePark.addEntertainment(rollerCoaster);
+        themePark.addEntertainment(candyflossStall);
+        themePark.addEntertainment(iceCreamStall);
+        themePark.addEntertainment(tobaccoStall);
+        assertEquals(7, themePark.getEntertainmentCount());
+    }
+
+
+
+}
